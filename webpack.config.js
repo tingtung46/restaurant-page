da4273d5ada67d2fc9908eb99b572be3120d66d8
root <132725436+tingtung46@users.noreply.github.com>
2024-04-21 20:36:08 +0700

    Add image loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,12 @@ module.exports = {
         template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
